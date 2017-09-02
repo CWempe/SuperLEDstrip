0e0541f9af9490a0d417f9465462c31aa4ce66f4
Christoph Wempe <christoph@wempe.net>
2017-09-02 19:18:06 +0200

    outsource dht to dht.h

--- SuperLEDstrip.ino
+++ SuperLEDstrip.ino
@@ -8,6 +8,13 @@
 #include <Homie.h>
 
 /*
+ * Function prototypes
+ */
+void setTextAllSensors(void);
+void Fire2012(void);
+void arc_pulse(void);
+
+/*
  *  FastLED
  */
 FASTLED_USING_NAMESPACE 
@@ -28,16 +35,6 @@ elapsedMillis timeElapsed2;
  */
 SoftwareSerial HMISerial(NEXTION_TX, NEXTION_RX);
 
-/*
- * DHT sensor
- */
-bool DHT_POWER_STATE;           // power state of DHT
-DHT dht(DHT_DATA_PIN, DHT_TYPE);
-elapsedMillis timeElapsedDHT;
-float fTemp         = 0;
-char  sTemp[10]     = "";
-float fHumid        = 0;
-char  sHumid[10]    = "";
 
 /*
  * define HomieNodes
@@ -48,50 +45,8 @@ HomieNode lightNode("light", "switch");
 //HomieNode lightNode("brightness", "brightness");
 
 
-/*
- * ############################
- * ### Enviroment Functions ###
- * ############################
- */
-void setupDHT()
-{
-  pinMode(DHT_POWER_PIN, OUTPUT);
-  digitalWrite(DHT_POWER_PIN, HIGH);
-  DHT_POWER_STATE = true;
-  timeElapsedDHT = 0;
-}
+#include "dht.h"
 
-void loopDHT()
-{
-  // When sensor is on and intervall time is reached, then power on
-  if ( !DHT_POWER_STATE && (timeElapsedDHT/1000) > DHT_READ_INTERVAL )
-  {
-    digitalWrite(DHT_POWER_PIN, HIGH);
-    DHT_POWER_STATE = true;
-    timeElapsedDHT = 0;
-  }
-
-  // When seonsor is on and warmup time is reached, then read data and power off
-  if ( DHT_POWER_STATE && (timeElapsedDHT/1000) > DHT_WARMUP )
-  {
-    fTemp = dht.readTemperature() + DHT_OFFSET_TEMP;
-    dtostrf(fTemp, 4, 1, sTemp);
-    sprintf(sTemp, "%s C", sTemp);
-    temperatureNode.setProperty("degrees").send(String(fTemp));
-
-    fHumid = dht.readHumidity() + DHT_OFFSET_HUMID;
-    dtostrf(fHumid, 3, 0, sHumid);
-    sprintf(sHumid, "%s %", sHumid);
-    humidityNode.setProperty("percentage").send(String(fHumid));
-    
-    setTextAllSensors();
-    
-    digitalWrite(DHT_POWER_PIN, LOW);
-    DHT_POWER_STATE = false;
-    timeElapsedDHT = 0;
-
-  }
-}
 
 
 /*
---
+++ dht.h
@@ -0,0 +1,52 @@
+/*
+ * DHT sensor
+ */
+bool DHT_POWER_STATE;           // power state of DHT
+DHT dht(DHT_DATA_PIN, DHT_TYPE);
+elapsedMillis timeElapsedDHT;
+float fTemp         = 0;
+char  sTemp[10]     = "";
+float fHumid        = 0;
+char  sHumid[10]    = "";
+
+
+
+void setupDHT()
+{
+  pinMode(DHT_POWER_PIN, OUTPUT);
+  digitalWrite(DHT_POWER_PIN, HIGH);
+  DHT_POWER_STATE = true;
+  timeElapsedDHT = 0;
+}
+
+void loopDHT()
+{
+  // When sensor is on and intervall time is reached, then power on
+  if ( !DHT_POWER_STATE && (timeElapsedDHT/1000) > DHT_READ_INTERVAL )
+  {
+    digitalWrite(DHT_POWER_PIN, HIGH);
+    DHT_POWER_STATE = true;
+    timeElapsedDHT = 0;
+  }
+
+  // When seonsor is on and warmup time is reached, then read data and power off
+  if ( DHT_POWER_STATE && (timeElapsedDHT/1000) > DHT_WARMUP )
+  {
+    fTemp = dht.readTemperature() + DHT_OFFSET_TEMP;
+    dtostrf(fTemp, 4, 1, sTemp);
+    sprintf(sTemp, "%s C", sTemp);
+    temperatureNode.setProperty("degrees").send(String(fTemp));
+
+    fHumid = dht.readHumidity() + DHT_OFFSET_HUMID;
+    dtostrf(fHumid, 3, 0, sHumid);
+    sprintf(sHumid, "%s %", sHumid);
+    humidityNode.setProperty("percentage").send(String(fHumid));
+    
+    setTextAllSensors();
+    
+    digitalWrite(DHT_POWER_PIN, LOW);
+    DHT_POWER_STATE = false;
+    timeElapsedDHT = 0;
+
+  }
+}
