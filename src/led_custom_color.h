90b2c6086ac3cfd166580fbccea92953a1c0b624
Christoph Wempe <christoph@wempe.net>
2018-10-14 16:39:03 +0200

    Use seperate functions to update custom colors

--- src/led_custom_color.h
+++ src/led_custom_color.h
@@ -1,42 +1,77 @@
-bool lightCustomColorHandler(HomieRange range, String value) {
-    if (value == "off") {
-        customColor.red   = 0;
-        customColor.green = 0;
-        customColor.blue  = 0;
-        // customColor.white = 0;
-        lightNode.setProperty("customColor").send("0,0,0");
-    } else if (value == "on") {
-        customColor.red   = 255;
-        customColor.green = 255;
-        customColor.blue  = 255;
-        // customColor.white = 255;
-        lightNode.setProperty("customColor").send("255,255,255");
+void updateCustomColor() {
+       // set led strip with new color
+       // customColor = CRGB(customColor.red, customColor.green, customColor.blue);
+       setupOneColor(customColor);
+
+       char customColorString[15] = "";
+       sprintf(customColorString, "%i,%i,%i", customColor.red, customColor.green, customColor.blue);
+       // sprintf(customColorString, "%i,%i,%i,%i", customColor.red, customColor.green, customColor.blue, customColor.white);
+       
+       lightNode.setProperty("customColor").send(customColorString);
+}
+
+void updateCustomColorRed(uint8_t red, bool updateRGB = true) {
+    customColor.red = red;
+
+    // Send updated RGB values to mqtt and display.
+    // But only do so if you do not want to change other colors at the same time.
+    // In that case only send update when you changed the last color.
+    if ( updateRGB == true) {
+        updateCustomColor();
+    }
+}
+
+void updateCustomColorGreen(uint8_t green, bool updateRGB = true) {
+    customColor.green = green;
+
+    // Send updated RGB values to mqtt and display.
+    // But only do so if you do not want to change other colors at the same time.
+    // In that case only send update when you changed the last color.
+    if ( updateRGB == true) {
+        updateCustomColor();
+    }
+}
+
+void updateCustomColorBlue(uint8_t blue, bool updateRGB = true) {
+    customColor.blue = blue;
+
+    // Send updated RGB values to mqtt and display.
+    // But only do so if you do not want to change other colors at the same time.
+    // In that case only send update when you changed the last color.
+    if ( updateRGB == true) {
+        updateCustomColor();
+    }
+}
+
+
+bool lightCustomColorHandler (HomieRange range, String value) {
+    uint8_t red   = 0;
+    uint8_t green = 0;
+    uint8_t blue  = 0;
+
+    if (value == "on") {
+        red   = 255;
+        green = 255;
+        blue  = 255;
+    } else if (value == "off") {
+        red   = 0;
+        green = 0;
+        blue  = 0;
     } else {
-        Serial.print (value);
         // split string at every "," and store in proper variable
         // convert final result to integer
-        uint8_t firstIndex = value.indexOf(',');
+        uint8_t firstIndex  = value.indexOf(',');
         uint8_t secondIndex = value.indexOf(',', firstIndex+1);
-        uint8_t thirdIndex = value.indexOf(',', secondIndex+1);
+        uint8_t thirdIndex  = value.indexOf(',', secondIndex+1);
 
-        customColor.red   = value.substring(0,firstIndex).toInt();
-        customColor.green = value.substring(firstIndex+1,secondIndex+1).toInt();
-        customColor.blue  = value.substring(secondIndex+1,thirdIndex+1).toInt();
-
-        // check is there is a fourth value provided
-        // if ( thirdIndex != 255 ) {
-        //     customColor.white = value.substring(secondIndex+1).toInt();
-        // }
-        
-        // set led strip with new color
-        // customColor = CRGB(customColor.red, customColor.green, customColor.blue);
-        setupOneColor(customColor);
-
-        char customColorString[15] = "";
-        sprintf(customColorString, "%i,%i,%i", customColor.red, customColor.green, customColor.blue);
-        // sprintf(customColorString, "%i,%i,%i,%i", customColor.red, customColor.green, customColor.blue, customColor.white);
-        
-        lightNode.setProperty("customColor").send(customColorString);
+        red   = value.substring(0,firstIndex).toInt();
+        green = value.substring(firstIndex+1,secondIndex+1).toInt();
+        blue  = value.substring(secondIndex+1,thirdIndex+1).toInt(); 
     }
+    
+    // update color values and send updated values with the last update.
+    updateCustomColorRed(red, false);
+    updateCustomColorGreen(green, false);
+    updateCustomColorBlue(blue, true);
     return true;
 }
