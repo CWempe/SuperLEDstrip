6b5fb60d652caba019a65d45830db42f6866b9c5
Christoph Wempe <christoph@wempe.net>
2019-08-24 22:12:34 +0200

    Download american flag svg

--- Display/exported/get_images.sh
+++ Display/exported/get_images.sh
@@ -4,5 +4,8 @@ WORKDIR="$(readlink -f $(dirname $(readlink -f "$0")))"
 IMAGEDIR="${WORKDIR}/external_images"
 
 
-# doenload club logos
+# download flags
+curl https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg -o ${IMAGEDIR}/flag_USA.svg
+
+# download club logos
 curl https://upload.wikimedia.org/wikipedia/commons/6/66/HSV-Logo.svg -o ${IMAGEDIR}/HSV-Logo.svg
